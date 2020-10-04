fcaa081c288ff8d0c6563995327a6453332c93e5
Larry Faudree <larryfaudree@Larrys-MBP.attlocal.net>
2020-10-04 14:22:41 -0400

    update requires

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const { freeze } = require('./helpers')
 
 const Wrappers = {
-  Identity: require('./Identity'),
-  IO: require('./IO'),
-  Task: require('./Task'),
+  Identity: require('./Identity/Identity'),
+  IO: require('./IO/IO'),
+  Task: require('./Task/Task'),
 }
 
 module.exports = freeze(Wrappers)
